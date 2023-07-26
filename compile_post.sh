f442c5f38a3c271df0f890b84cf385b47a156367
gongxini <hyigong1@jhu.edu>
2023-07-26 07:33:57 -0600

    update compile file for postgresql

--- compile_post.sh
+++ compile_post.sh
@@ -3,10 +3,10 @@
 mkdir -p dist
 cd 1.0.19
 ./autogen.sh
-./configure  --prefix=$(pwd)/../dist --with-pgsql --without-mysql
+./configure  --prefix=$(pwd)/../dist --with-pgsql --with-pgsql-includes=${PSANDBOX_POSTGRES_DIR}/include --with-pgsql-libs=${PSANDBOX_POSTGRES_DIR}/lib --without-mysql
 
 make -j4
 make install
 cd ..
-echo "export POSTGRES_SYSBENCH_DIR=`pwd`/dist/share/sysbench" >> $HOME/.bashrc
+echo "export POSTGRES_SYSBENCH_DIR=`pwd`/dist" >> $HOME/.bashrc
 
